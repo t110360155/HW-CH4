89a7630c8c04ae9c6815212d54bdda71c7d4583d
Paula <t110360155@ntut.org.tw>
2023-11-30 18:19:30 +0800

    20231130

---
+++ S8_110360155.java
@@ -0,0 +1,27 @@
+public class S8_110360155
+{
+    public static void main (String[]args)
+    {
+        Car car1 = new Car();
+        Car car2 = new Car();
+        
+        Car car3;
+        car3 = car1;
+        
+        System.out.println("carl與 car2相同"+car1.equals(car2));
+        System.out.println("carl與 car3相同"+car1.equals(car3));
+    }
+}
+
+class Car
+{
+    protected int num;
+    protected double gas;
+    
+    public Car()
+    {
+        num = 0;
+        gas = 0.0;
+        System.out.println("生產了車子");
+    }
+}
